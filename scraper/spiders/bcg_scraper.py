751901d73591727765f033849632b4c8e9aa92bf
jeffrey chen <kunyuzheng@jeffreys-mac-2.mynetworksettings.com>
2024-07-11 01:06:29 -0400

    Initial commit

---
+++ scraper/spiders/bcg_scraper.py
@@ -0,0 +1,55 @@
+import scrapy
+from bs4 import BeautifulSoup
+from scrapy.utils.project import get_project_settings
+
+class MySpider(scrapy.Spider):
+    name = 'bcg_scraper'
+    start_urls = [
+        'https://bcg.com',
+    ]
+    allowed_domains = [
+        'bcg.com',
+    ]
+
+    # Keywords to filter links
+    link_keywords = ['service', 'case-studies', 'client-testimonials', 'leadership', 'insight']
+
+    # Keywords to extract data
+    data_keywords = ['service', 'case stud', 'quote', 'thought leadership', 'insight']
+
+    def parse(self, response):
+        # Extract all links on the page
+        links = response.css('a::attr(href)').getall()
+        
+        # Filter and follow links based on keywords
+        for link in links:
+            if any(keyword in link for keyword in self.link_keywords):
+                yield response.follow(link, self.parse)
+
+        # Use BeautifulSoup to parse the page content
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Dictionary to store the extracted data
+        data_dict = {keyword: [] for keyword in self.data_keywords}
+        found_any_keyword = False
+
+        # Extract data based on keywords
+        for keyword in self.data_keywords:
+            elements = soup.find_all(string=lambda text: text and keyword.lower() in text.lower())
+            for element in elements:
+                parent = element.find_parent()
+                data_text = parent.get_text(separator=' ', strip=True)
+                data_dict[keyword].append(data_text)
+                found_any_keyword = True
+
+        # If any data keywords were found, print the dictionary
+        if found_any_keyword:
+            print(f"URL: {response.url}")
+            print(data_dict)
+            print('---')
+
+            # Yield the data dictionary (optional)
+            yield {
+                "url": response.url,
+                "data": data_dict
+            }
---
+++ scraper/spiders/dubai_scraper.py
@@ -0,0 +1,67 @@
+import scrapy
+from bs4 import BeautifulSoup
+
+class MySpider(scrapy.Spider):
+
+    custom_settings = {
+        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'ROBOTSTXT_OBEY': False,  # Disable robots.txt obeying for testing
+    }
+    name = 'dubai_scraper'
+    start_urls = [
+        'https://gulfnews.com/',
+        'https://khaleejtimes.com',
+        'https://thenationalnews.com',
+    ]
+    allowed_domains = [
+        'gulfnews.com',
+        'khaleejtimes.com',
+        'thenationalnews.com',
+    ]
+
+    # Keywords to filter links
+    link_keywords = ['market', 'financ', 'analysis', 'trend', 'report', 'industry', 'price']
+
+    # Keywords to extract data
+    data_keywords = ['market', 'financ', 'analysis', 'trend', 'report', 'industry']
+
+    def parse(self, response):
+        # Extract all links on the page
+        links = response.css('a::attr(href)').getall()
+        
+        # Filter links based on keywords
+        filtered_links = [link for link in links if any(keyword in link for keyword in self.link_keywords)]
+        
+        # Visit each filtered link once
+        for link in filtered_links:
+        #for link in links:
+            yield response.follow(link, self.parse_link)
+
+    def parse_link(self, response):
+        # Use BeautifulSoup to parse the page content
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Dictionary to store the extracted data
+        data_dict = {keyword: [] for keyword in self.data_keywords}
+        found_any_keyword = False
+
+        # Extract data based on keywords
+        for keyword in self.data_keywords:
+            elements = soup.find_all(string=lambda text: text and keyword.lower() in text.lower())
+            for element in elements:
+                parent = element.find_parent()
+                data_text = parent.get_text(separator=' ', strip=True)
+                data_dict[keyword].append(data_text)
+                found_any_keyword = True
+
+        # If any data keywords were found, print the dictionary
+        if found_any_keyword:
+            print(f"URL: {response.url}")
+            print(data_dict)
+            print('---')
+
+            # Yield the data dictionary (optional)
+            yield {
+                "url": response.url,
+                "data": data_dict
+            }
