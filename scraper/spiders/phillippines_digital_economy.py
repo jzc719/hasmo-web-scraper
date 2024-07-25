e033b7d8fd32b16b25f33fa070a23bd491573661
jzc719 <97258997+jzc719@users.noreply.github.com>
2024-07-25 12:40:16 -0400

    Add files via upload

---
+++ scraper/spiders/phillippines_digital_economy.py
@@ -0,0 +1,73 @@
+import scrapy
+from bs4 import BeautifulSoup
+
+# Author: Jeffrey Chen
+
+class Phillippines_Digital_Economy(scrapy.Spider):
+
+    custom_settings = {
+        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'ROBOTSTXT_OBEY': False,  # Disable robots.txt obeying for testing
+    }
+
+    # Name of the spider
+    # This is used when running in terminal, not the file name
+    name = 'phillippines_digital_economy'
+
+    # URL(s) to start crawling at
+    start_urls = [
+        'https://www.techinasia.com',
+        'https://www.e27.co',
+        'https://www.digitalnewsasia.com',
+        'https://www.fintechph.org',
+        'https://www.insurtechinsights.com'
+    ]
+
+    # To prevent offsite crawling
+    allowed_domains = [
+        'techinasia.com',
+        'e27.co',
+        'digitalnewsasia.com',
+        'fintechph.org',
+        'insurtechinsights.com'
+    ]
+
+    # Keywords to extract data
+    data_keywords = [ 'insur', 'digital', 'econom', 'market', 'tech', 'financ', 'data']
+
+    def parse(self, response):
+        # Extract all links on the page
+        links = response.css('a::attr(href)').getall()
+        
+        # Visit each link once
+        for link in links:
+            yield response.follow(link, self.parse_link)
+
+    def parse_link(self, response):
+        # Use BeautifulSoup to parse the page content
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Dictionary to store the extracted data
+        data_dict = {keyword: [] for keyword in self.data_keywords}
+        found_any_keyword = False
+
+        # Extract data based on keywords
+        for keyword in self.data_keywords:
+            elements = soup.find_all(string=lambda text: text and keyword.lower() in text.lower())
+            for element in elements:
+                parent = element.find_parent()
+                data_text = parent.get_text(separator=' ', strip=True)
+                data_dict[keyword].append(data_text)
+                found_any_keyword = True
+
+        # If any data keywords were found, print the dictionary
+        if found_any_keyword:
+            print(f"URL: {response.url}")
+            print(data_dict)
+            print('---')
+
+            # Yield the data dictionary 
+            yield {
+                "url": response.url,
+                "data": data_dict
+            }
---
+++ scraper/spiders/phillippines_regulatory_updates.py
@@ -0,0 +1,69 @@
+import scrapy
+from bs4 import BeautifulSoup
+
+# Author: Jeffrey Chen
+
+class Phillippines_Regulatory_Updates(scrapy.Spider):
+
+    custom_settings = {
+        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'ROBOTSTXT_OBEY': False,  # Disable robots.txt obeying for testing
+    }
+
+    # Name of the spider
+    # This is used when running in terminal, not the file name
+    name = 'phillippines_regulatory_updates'
+
+    # URL(s) to start crawling at
+    start_urls = [
+        'https://www.officialgazette.gov.ph',
+        'https://www.insurance.gov.ph',
+        'https://www.bsp.gov.ph/SitePages/Default.aspx'
+    ]
+
+    # To prevent offsite crawling
+    allowed_domains = [
+        'officialgazette.gov.ph',
+        'insurance.gov.ph',
+        'bsp.gov.ph'
+    ]
+
+    # Keywords to extract data
+    data_keywords = [ 'regulat', 'law', 'policy', 'complianc', 'standard', 'guideline', 'rule', 'mandate', 'control', 'requirement', 'licens', 'authorization', 'insurance']
+
+    def parse(self, response):
+        # Extract all links on the page
+        links = response.css('a::attr(href)').getall()
+        
+        # Visit each link once
+        for link in links:
+            yield response.follow(link, self.parse_link)
+
+    def parse_link(self, response):
+        # Use BeautifulSoup to parse the page content
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Dictionary to store the extracted data
+        data_dict = {keyword: [] for keyword in self.data_keywords}
+        found_any_keyword = False
+
+        # Extract data based on keywords
+        for keyword in self.data_keywords:
+            elements = soup.find_all(string=lambda text: text and keyword.lower() in text.lower())
+            for element in elements:
+                parent = element.find_parent()
+                data_text = parent.get_text(separator=' ', strip=True)
+                data_dict[keyword].append(data_text)
+                found_any_keyword = True
+
+        # If any data keywords were found, print the dictionary
+        if found_any_keyword:
+            print(f"URL: {response.url}")
+            print(data_dict)
+            print('---')
+
+            # Yield the data dictionary 
+            yield {
+                "url": response.url,
+                "data": data_dict
+            }
